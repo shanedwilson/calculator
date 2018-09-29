10bc30b6e291b74a893d1f08d13b52dd35d4c096
Shane D Wilson <me@shanedwilson.com>
2018-09-29 10:18:10 -0500

    multiply works

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('hello');
+const printToDom = (stringToPrint, divId) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += stringToPrint;
+};
+
+const multiply = (n1, n2) => {
+  return n1 * n2;
+};
+
+const calculate = (num1, num2, mathType) => {
+  let answer = 0;
+  switch (mathType) {
+    case "multiply":
+      answer = multiply(num1, num2);
+      break;
+    case "divide":
+      answer = num1 / num2;
+      break;
+    case "add":
+      answer = num1 + num2;
+      break;
+    case "subtract":
+      answer = num1 - num2;
+      break;
+    default:
+      answer = "nope";
+  }
+  printToDom(answer, "result");
+};
+
+calculate(4, 2, "multiply"); //8
