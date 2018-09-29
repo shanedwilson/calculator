5c8348f35fb9a69a5d75b6246b7f305ef1d69e4b
Shane D Wilson <me@shanedwilson.com>
2018-09-29 11:18:48 -0500

    broke out maths and util functions

---
+++ javascripts/helpers/maths.js
@@ -0,0 +1,7 @@
+
+
+const multiply = (n1, n2) => {
+    return n1 * n2;
+  };
+
+export {multiply};  
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,5 @@
-const printToDom = (stringToPrint, divId) => {
-  const selectedDiv = document.getElementById(divId);
-  selectedDiv.innerHTML += stringToPrint;
-};
-
-const multiply = (n1, n2) => {
-  return n1 * n2;
-};
+import {printToDom} from './helpers/util.js';
+import {multiply} from './helpers/maths.js';
 
 const calculate = (num1, num2, mathType) => {
   let answer = 0;
